18a63578ea8efb565a41943c6d0274037d0b331d
cabrel Ange <cabrel.lene@yahoo.com>
2025-09-03 22:02:49 -0400

    mis en place

---
+++ immigration-canada-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'immigration-canada-app',
+  webDir: 'dist'
+};
+
+export default config;
